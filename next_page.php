c175d71ce8d19b8c9b3856f5706a0e59ef524edb
IzzatMaswan <2020866462@student.uitm.edu.my>
2022-06-24 22:36:13 +0800

    c7

--- next_page.php
+++ next_page.php
@@ -2,14 +2,16 @@
     session_start();
     echo "<html>";
 
-    $sid = session_id();
-    echo "Season ID returned by session_id(): ".$sid." \n ";
-    $sid = SID;
-    echo "Season ID returned by SID: ".$sid."\n";
+    $queryString = $_SERVER["QUERY_STRING"];
+    echo "Query string of the incoming URL: ".$queryString."\n ";
+
+    echo "Cookies received: \n";
+    foreach ($_COOKIE as $name => $value ) {
+        echo "$name = $value\n";
+    }
 
     $myLogin = $_SESSION["myLogin"];
     echo "Value of myLogin hs been retrieve: ".$myLogin."\n ";
-
     $myColor = $_SESSION["myColor"];
     echo "Value of MyColor has been retrieved: ".$myColor."\n ";
 
